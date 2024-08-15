d3949cab4d613c710649b40cdb1d577305ca45c7
alexchtst-01 <acinatra@gmail.com>
2024-08-15 15:52:38 +0700

    fixing layout

--- template_library/src/app/sections/hero/heroVariant3.tsx
+++ template_library/src/app/sections/hero/heroVariant3.tsx
@@ -5,32 +5,34 @@ export default function Hero3() {
     <section className="w-full aspect-[1920/1080] flex flex-col justify-center items-center bg-gray-100 relative">
       {/* Ornament background */}
       <Image
-          src="/image/hero/hero3/bg.png"
-          alt="Background"
-          height={10000}
-          width={10000}
-          className="w-[100vw] h-[56.25vw] object-cover z-[0] absolute"
-        />
+        src="/image/hero/hero3/bg.png"
+        alt="Background"
+        height={10000}
+        width={10000}
+        className="w-[100vw] h-[56.25vw] object-cover z-[0] absolute"
+      />
       <div className="w-full h-[56.25vw] z-[1] gap-y-[5vw] p-[2.083vw]">
         {/* navbar */}
-        <div className="w-full bg-white py-[0.41vw] px-[1.041vw] flex justify-between items-center rounded-full top-0">
-          <div className="flex gap-x-[0.625vw] text-[1.45vw] font-bold">
-            <Image
-              src="/image/hero/hero3/Union.png"
-              alt="Background"
-              height={10000}
-              width={10000}
-              className="w-[2vw] object-fill"
-            />
-            <p>Arachnova</p>
-          </div>
+        <div className="w-full flex justify-center items-center">
+          <div className="w-[80.26vw] bg-white py-[0.41vw] px-[1.041vw] flex justify-between items-center rounded-full top-0">
+            <div className="flex gap-x-[0.625vw] text-[1.45vw] font-bold">
+              <Image
+                src="/image/hero/hero3/Union.png"
+                alt="Background"
+                height={10000}
+                width={10000}
+                className="w-[2vw] object-fill"
+              />
+              <p className="text-[20px]">Arachnova</p>
+            </div>
 
-          <div className="flex gap-x-3 items-center text-[0.781vw]">
-            <a href="">Home</a>
-            <a href="">About Us</a>
-            <a href="">Pricing</a>
-            <a href="">Page</a>
-            <button className="border-2 border-orange-brown text-orange-brown p-2 rounded-md">Contact Us</button>
+            <div className="flex gap-x-3 items-center text-[0.781vw]">
+              <a href="">Home</a>
+              <a href="">About Us</a>
+              <a href="">Pricing</a>
+              <a href="">Page</a>
+              <button className="border-2 border-orange-brown text-orange-brown p-2 rounded-md">Contact Us</button>
+            </div>
           </div>
         </div>
         {/* navbar */}
