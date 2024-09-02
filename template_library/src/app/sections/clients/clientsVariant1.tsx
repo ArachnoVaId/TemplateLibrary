f4d49e6223515e0efdb0793beb80952a8a8e624b
ryanlukito <ryanlukito13@gmail.com>
2024-09-02 17:56:26 +0700

    msg: update responsive cta and client

--- template_library/src/app/sections/clients/clientsVariant1.tsx
+++ template_library/src/app/sections/clients/clientsVariant1.tsx
@@ -1,117 +1,68 @@
 import Image from "next/image";
 
 const sources = [
-  { src: "/image/clients/reddit.png", width: "w-[6.52vw]", height: "h-[3vw]" },
+  {
+    src: "/image/clients/reddit.png",
+    width: "md:w-[6.52vw] w-[23.065vw]",
+    height: "md:h-[3vw] h-[13.256vw]",
+  },
   {
     src: "/image/clients/spotify.png",
-    width: "w-[6.823vw]",
-    height: "h-[2.083vw]",
+    width: "md:w-[9.107vw] w-[26.047vw]",
+    height: "md:h-[2.734vw] h-[7.907vw]",
   },
   {
     src: "/image/clients/google.png",
-    width: "w-[6.979vw]",
-    height: "h-[2.188vw]",
+    width: "md:w-[6.979vw] w-[25.581vw]",
+    height: "md:h-[2.188vw] h-[7.907vw]",
   },
   {
     src: "/image/clients/pinterest.png",
-    width: "w-[7.187vw]",
-    height: "h-[1.771vw]",
+    width: "md:w-[7.187vw] w-[26.512vw]",
+    height: "md:h-[1.771vw] h-[6.512vw]",
   },
   {
     src: "/image/clients/stripe.png",
-    width: "w-[9.107vw]",
-    height: "h-[4.341vw]",
+    width: "md:w-[9.107vw] w-[23.256vw]",
+    height: "md:h-[4.341vw] h-[10.93vw]",
   },
   {
     src: "/image/clients/google.png",
-    width: "w-[6.979vw]",
-    height: "h-[2.188vw]",
+    width: "md:w-[6.979vw] w-[25.581vw]",
+    height: "md:h-[2.188vw] h-[7.907vw]",
   },
   {
     src: "/image/clients/pinterest.png",
-    width: "w-[7.187vw]",
-    height: "h-[1.771vw]",
+    width: "md:w-[7.187vw] w-[26.512vw]",
+    height: "md:h-[1.771vw] h-[6.512vw]",
   },
   {
     src: "/image/clients/stripe.png",
-    width: "w-[9.107vw]",
-    height: "h-[4.341vw]",
+    width: "md:w-[9.107vw] w-[23.256vw]",
+    height: "md:h-[4.341vw] h-[10.93vw]",
   },
 ];
 
 export default function Clients1() {
   return (
-    <section className="w-full min-h-[20.833vw] p-8 flex flex-col justify-center items-center bg-white text-gray-800 relative">
-      <div className="w-[52.135vw] h-[2.5vw]">
-        <h1 className="text-[2.083vw] text-center font-bold">Our Client</h1>
+    <section className="w-full md:min-h-[20.833vw] md:aspect-ratio[1920/1080] aspect-ratio[430/459] p-8 flex flex-col justify-center items-center bg-white text-gray-800 relative">
+      <div className="md:w-[52.135vw] md:h-[2.5vw] w-[81.395vw] h-fit">
+        <h1 className="md:text-[2.083vw] text-[9.302vw] text-center font-bold">
+          Our Client
+        </h1>
       </div>
-      <div className="flex justify-center items-center">
+      <div className="md:flex md:justify-center md:items-center grid grid-cols-2 gap-[1.5vw] gap-x-[4vw] mt-[5vw] ">
         {sources.map((client, index) => (
-          <Image
-            key={index}
-            src={client.src}
-            alt="Ornament Background"
-            width={10000}
-            height={10000}
-            className={`${client.width} ${client.height} mt-[1vw] mx-[1vw]`}
-          />
+          <div key={index} className="flex justify-center items-center">
+            <Image
+              src={client.src}
+              alt="Ornament Background"
+              width={10000}
+              height={10000}
+              className={`${client.width} ${client.height} md:mt-[0.5vw] md:mx-[1vw]`}
+            />
+          </div>
         ))}
-        {/* <Image
-        src="/image/clients/spotify.png"
-        alt="Ornament Background"
-        width={10000}
-        height={10000}
-        className="w-[9.107vw] h-[2.734vw] mt-[2vw] mx-[0.5vw]"
-      /> */}
-        {/* <Image
-        src="/image/clients/google.png"
-        alt="Ornament Background"
-        width={10000}
-        height={10000}
-        className="w-[9.107vw] h-[2.734vw] mt-[2vw] mx-[0.5vw]"
-      />
-      <Image
-        src="/image/clients/pinterest.png"
-        alt="Ornament Background"
-        width={10000}
-        height={10000}
-        className="w-[9.107vw] h-[2.229vw] mt-[2vw] mx-[0.5vw]"
-      />
-      <Image
-        src="/image/clients/stripe.png"
-        alt="Ornament Background"
-        width={10000}
-        height={10000}
-        className="w-[9.107vw] h-[4.341vw] mt-[2vw] mx-[0.5vw]"
-      />
-      <Image
-        src="/image/clients/reddit.png"
-        alt="Ornament Background"
-        width={10000}
-        height={10000}
-        className="w-[9.107vw] h-[4.25vw] mt-[2vw] mx-[0.5vw]"
-      />
-      <Image
-        src="/image/clients/spotify.png"
-        alt="Ornament Background"
-        width={10000}
-        height={10000}
-        className="w-[9.107vw] h-[2.734vw] mt-[2vw] mx-[0.5vw]"
-      />
-      <Image
-        src="/image/clients/google.png"
-        alt="Ornament Background"
-        width={10000}
-        height={10000}
-        className="w-[9.107vw] h-[2.734vw] mt-[2vw] mx-[0.5vw]"
-      />
-      <Image
-        src="/image/clients/pinterest.png"
-        alt="Ornament Background"
-        width={10000}
-        height={10000}
-        className="w-[9.107vw] h-[2.229vw] mt-[2vw] mx-[0.5vw]"
-      /> */}
       </div>
     </section>
   );
--- template_library/src/app/sections/clients/clientsVariant3.tsx
+++ template_library/src/app/sections/clients/clientsVariant3.tsx
@@ -1,66 +1,68 @@
 import Image from "next/image";
 
 const sources1 = [
-  { src: "/image/clients/reddit.png", width: "w-[6.52vw]", height: "h-[3vw]" },
+  {
+    src: "/image/clients/reddit.png",
+    width: "md:w-[6.52vw] w-[13.337vw]",
+    height: "md:h-[3vw] h-[3.77vw]",
+  },
   {
     src: "/image/clients/spotify.png",
-    width: "w-[6.823vw]",
-    height: "h-[2.083vw]",
+    width: "md:w-[6.823vw] w-[11.212vw]",
+    height: "md:h-[2.083vw] h-[3.093vw]",
   },
   {
     src: "/image/clients/google.png",
-    width: "w-[6.979vw]",
-    height: "h-[2.188vw]",
+    width: "md:w-[6.979vw] w-[11.986vw]",
+    height: "md:h-[2.188vw] h-[3.384vw]",
   },
   {
     src: "/image/clients/pinterest.png",
-    width: "w-[7.187vw]",
-    height: "h-[1.771vw]",
+    width: "md:w-[7.187vw] w-[14.014vw]",
+    height: "md:h-[1.771vw] h-[4.447vw]",
   },
   {
     src: "/image/clients/stripe.png",
-    width: "w-[9.107vw]",
-    height: "h-[4.341vw]",
-  },
-  { src: "/image/clients/reddit.png", width: "w-[6.52vw]", height: "h-[3vw]" },
-  {
-    src: "/image/clients/spotify.png",
-    width: "w-[6.823vw]",
-    height: "h-[2.083vw]",
+    width: "md:w-[9.107vw] w-[13.337vw]",
+    height: "md:h-[4.341vw] h-[3.093vw]",
   },
 ];
 const sources2 = [
-  { src: "/image/clients/reddit.png", width: "w-[6.52vw]", height: "h-[3vw]" },
+  {
+    src: "/image/clients/reddit.png",
+    width: "md:w-[6.52vw] w-[13.119vw]",
+    eight: "md:h-[3vw] h-[3.423vw]",
+  },
   {
     src: "/image/clients/spotify.png",
-    width: "w-[6.823vw]",
-    height: "h-[2.083vw]",
+    width: "md:w-[6.823vw] w-[7.795vw]",
+    height: "md:h-[2.083vw] w-[4.849vw]",
   },
   {
     src: "/image/clients/google.png",
-    width: "w-[6.979vw]",
-    height: "h-[2.188vw]",
+    width: "md:w-[6.979vw] w-[9.412vw]",
+    height: "md:h-[2.188vw] h-[3.802vw]",
   },
   {
     src: "/image/clients/pinterest.png",
-    width: "w-[7.187vw]",
-    height: "h-[1.771vw]",
+    width: "md:w-[7.187vw] w-[6.653vw]",
+    height: "md:h-[1.771vw] h-[4.184vw]",
   },
   {
     src: "/image/clients/stripe.png",
-    width: "w-[9.107vw]",
-    height: "h-[4.341vw]",
+    width: "md:w-[9.107vw] w-[10.456vw]",
+    height: "md:h-[4.341vw] h-[2.756vw]",
   },
 ];
 
 export default function Clients3() {
   return (
-    <section className="w-full min-h-[20.885vw] p-8 flex flex-col justify-center items-center bg-white text-gray-800 relative">
-      <h1 className="w-[91.667vw] h-[2.292vw] text-center font-bold text-[2.083vw]">
+    <section className="w-full md:min-h-[20.885vw] md:aspect-ratio[1920/1080] aspect-ratio[430/321.97] p-8 flex flex-col justify-center items-center bg-white text-gray-800 relative">
+      <h1 className="md:w-[91.667vw] md:h-[2.292vw] w-[81.395vw] h-[8.14vw] text-center font-bold md:text-[2.083vw] text-[7.442vw]">
         Our Top Partners
       </h1>
       <div className="flex flex-col">
-        <div className="flex justify-center items-center mt-[2vw]">
+        <div className="flex justify-center items-center md:mt-[2vw] mt-[5vw]">
           {sources1.map((client, index) => (
             <Image
               key={index}
