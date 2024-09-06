48d80095c9bb1a9fc89b028d59861da7956086c1
alexchtst-01 <acinatra@gmail.com>
2024-09-07 03:24:20 +0700

    feat: create serviceVariant5

--- template_library/src/app/sections/services/serviceVariant5.tsx
+++ template_library/src/app/sections/services/serviceVariant5.tsx
@@ -24,15 +24,17 @@ interface cardProps {
 
 function CardService({ image_path, type, title, description }: cardProps) {
   return (
-    <div className="w-[22.104vw] h-[31.688vw] p-[0.5vw] font-roboto shadow-lg rounded-xl m-[1vw]">
-      <Image alt="service image" src={image_path} width={10000} height={10000} className="w-[20.104vw] h-[11.458vw] mb-[1vw]" />
-      <p className="font-bold text-[0.833vw] mb-[0.5vw]">{type}</p>
-      <h1 className="font-bold text-[1.25vw] mb-[0.5vw]">{title}</h1>
-      <p className="text-[0.938vw] mb-[2vw]">{description}</p>
-      <button className="w-full flex items-center gap-x-[0.8vw] p-[1vw] text-[0.833vw] font-bold text-white bg-blue-500 rounded-md justify-center">
-        <p>Button Text</p>
-        <FaArrowRight />
-      </button>
+    <div className="md:w-[22.104vw] w-[68.837vw] md:h-[31.688vw] h-[87.209vw] md:p-[0.5vw]  font-roboto shadow-lg rounded-xl m-[1vw]">
+      <Image alt="service image" src={image_path} width={10000} height={10000} className="md:w-[20.104vw] w-[68.837vw] md:h-[11.458vw] h-[35.581vw] mb-[1vw] md:p-0 p-[1vw] md:rounded-none rounded-lg" />
+      <p className="font-bold md:text-[0.833vw] text-[2.593vw] md:mb-[0.5vw] mb-[3vw] md:p-0 px-[5vw] md:mt-0 mt-[2vw]">{type}</p>
+      <h1 className="font-bold md:text-[1.25vw] text-[3.888vw] md:mb-[0.5vw] mb-[3vw] md:p-0 px-[5vw]">{title}</h1>
+      <p className="md:text-[0.938vw] text-[2.916vw] md:mb-[2vw] mb-[3vw] md:p-0 px-[5vw]">{description}</p>
+      <div className="md:px-0 px-[5vw]">
+        <button className="w-full flex items-center gap-x-[0.8vw] p-[1vw] md:text-[0.833vw] text-[2.593vw] font-bold text-white bg-blue-500 rounded-md justify-center">
+          <p>Button Text</p>
+          <FaArrowRight />
+        </button>
+      </div>
     </div>
   )
 }
@@ -51,31 +53,36 @@ export default function Service5() {
     }
   };
   return (
-    <section className="w-full aspect-[1920/1080] bg-[rgba(246,248,247,1)] py-[3.125vw] flex flex-col justify-center items-center">
+    <section className="w-full md:aspect-[1920/1080] aspect-[430/1080] bg-[rgba(246,248,247,1)] py-[3.125vw] flex flex-col items-center">
       {/* title */}
       <div className="mx-[4.166vw] text-center flex flex-col gap-y-[1vw]">
-        <h1 className="font-bold text-[2.917vw] gradient-text">Our Products</h1>
-        <p className="text-[1.0416vw] text-[rgba(71,85,105,1)] w-[46.875vw]">
+        <h1 className="font-bold md:text-[2.917vw] text-[3vw] gradient-text">Our Products</h1>
+        <p className="md:text-[1.0416vw] text-[3.163vw] text-[rgba(71,85,105,1)] md:w-[46.875vw] w-full">
         Et pulvinar nec interdum integer id urna molestie porta nullam. A, donec ornare sed turpis pulvinar purus maecenas quam a. Erat porttitor pharetra sed in mauris elementum sollicitudin.
         </p>
       </div>
       {/* title */}
 
       {/* content */}
-      <div className="ml-[4.166vw] mt-[2.6875vw] flex gap-x-[2vw] justify-between">
+      <div className="md:ml-[4.166vw] mt-[2.6875vw] flex gap-x-[2vw] justify-between">
         <button onClick={handlePrev}>
-          <FaChevronLeft className="text-[1.2vw]" />
+          <FaChevronLeft className="md:text-[1.2vw] text-[2.5vw]" />
         </button>
         <div className="flex gap-x-[0.9vw] p-[1vw]">
           <Swiper
             className="w-[72.4791vw]"
             ref={swiperRef}
             spaceBetween={10}
-            slidesPerView={3}
+            slidesPerView={1}
             modules={[Navigation]}
             navigation={{
               prevEl: '.swiper-button-prev',
               nextEl: '.swiper-button-next',
+            }}
+            breakpoints={{
+              1024:{
+                slidesPerView: 3
+              }
             }}
           >
             {data.map((d, index) => (
@@ -86,7 +93,7 @@ export default function Service5() {
           </Swiper>
         </div>
         <button onClick={handleNext}>
-          <FaChevronRight className="text-[1.2vw]" />
+          <FaChevronRight className="md:text-[1.2vw] text-[2.5vw]" />
         </button>
       </div>
       {/* content */}
