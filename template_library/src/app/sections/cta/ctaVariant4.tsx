0924c8700b1b6d499c4078d5ee5210f9ef1f121a
ryanlukito <ryanlukito13@gmail.com>
2024-09-07 21:12:07 +0700

    msg: update aboutUs and cta

--- template_library/src/app/sections/cta/ctaVariant4.tsx
+++ template_library/src/app/sections/cta/ctaVariant4.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function cta4() {
   return (
-    <section className="w-full md:min-h-[23.49vw] md:aspect-ratio[1920/1080] aspect-ratio[430/258] p-8 flex flex-col justify-center items-center bg-[#161C2D] text-gray-800 relative">
+    <section className="w-full md:min-h-[23.49vw] md:aspect-ratio[1920/1080] aspect-ratio[430/258] p-8 flex flex-col justify-center items-center relative bg-[#161C2D] bg-[url('/image/cta/BG.png')] bg-cover">
       <div className="md:w-[30.469vw] md:h-[12.344vw] w-[89.07vw] h-[36.084vw] flex flex-col items-center justify-center">
         <h1 className="text-white text-center font-bold text-[9.135vw] md:text-[3.125vw] md:leading-[3.5vw] leading-[10vw]">
           Ready to have a decorated lifestyle?
