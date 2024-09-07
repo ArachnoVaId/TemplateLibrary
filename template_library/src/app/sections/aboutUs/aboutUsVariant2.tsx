d876ad9edc11d975ffbe2663966d635d3d3d23e4
ryanlukito <ryanlukito13@gmail.com>
2024-09-07 20:06:02 +0700

    msg: update aboutUs

--- template_library/src/app/sections/aboutUs/aboutUsVariant2.tsx
+++ template_library/src/app/sections/aboutUs/aboutUsVariant2.tsx
@@ -3,28 +3,24 @@ import Image from "next/image";
 export default function AboutUs2() {
   return (
     <section className="w-full md:min-h-[31.563vw] md:aspect-ratio[1920/1080] aspect-ratio[430/742] p-8 flex flex-col justify-center items-center bg-white text-gray-800 relative ">
-      <div className="w-[84.884vw] h-[141.46vw] md:w-[65.729vw] md:h-[19.583vw] flex justify-center items-center">
+      <div className="w-[84.884vw] h-[141.46vw] md:w-[65.729vw] md:h-[19.583vw] flex justify-center items-center md:items-start">
         <div className="w-[23.385vw] h-[16.042vw]">
-          <h1 className="font-bold text-[1.875vw] text-[#C77B2C] order-1">
-            About us
-          </h1>
-          <div className="order-3">
-            <p className="text-[1.042vw]">
-              Lorem ipsum dolor sit amet consectetur. <br />
-              Nullam elit adipiscing tincidunt id. Sed <br />
-              scelerisque in eu auctor. Lectus enim id et <br />
-              eget risus est ipsum morbi sit. Cursus tortor in <br />
-              et at viverra fermentum.
+          <h1 className="font-bold text-[1.875vw] text-[#C77B2C]">About us</h1>
+          <div className="">
+            <p className="text-[1.042vw] text-justify">
+              Lorem ipsum dolor sit amet consectetur. Nullam elit adipiscing
+              tincidunt id. Sed scelerisque in eu auctor. Lectus enim id et eget
+              risus est ipsum morbi sit. Cursus tortor in et at viverra
+              fermentum.
             </p>
-            <p className="mt-[1vw] text-[1.042vw]">
-              Orci tellus fames pharetra elit fermentum <br />
-              magna morbi donec. Pulvinar quis blandit <br />
-              sollicitudin ut lectus cursus. Ipsum luctus duis <br />
-              vitae venenatis. Amet.
+            <p className="text-[1.042vw] text-justify">
+              Orci tellus fames pharetra elit fermentum magna morbi donec.
+              Pulvinar quis blandit sollicitudin ut lectus cursus. Ipsum luctus
+              duis vitae venenatis. Amet.
             </p>
           </div>
         </div>
-        <div className="ml-[3vw] order-2">
+        <div className="ml-[3vw]">
           <Image
             src="/image/aboutUs/aboutUs2/fotoKopi.png"
             alt="Ornament Background"
--- template_library/src/app/sections/aboutUs/aboutUsVariant3.tsx
+++ template_library/src/app/sections/aboutUs/aboutUsVariant3.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function AboutUs3() {
   return (
     <section className="w-full md:min-h-[37.188vw] md:aspect-ratio[1920/1080] aspect-ratio[430/714] p-8 flex flex-col justify-center items-center bg-white text-gray-800 relative">
-      <div className="md:w-[66.615vw] md:h-[25.208vw] w-[184.065vw] h-[571.51] flex md:flex-row flex-col justify-center items-center">
+      <div className="md:w-[66.615vw] md:h-[25.208vw] w-[93.135vw] h-[571.51] flex md:flex-row flex-col justify-center items-center">
         <div className="md:w-[31.25vw] md:h-[24.167vw] w-[93.135vw] h-[72.026vw] flex justify-between">
           <div className="md:flex md:items-end">
             <div className="md:w-[9.861vw] md:h-[20vw] w-[29.391vw] h-[72.026vw] rounded-[1.042vw] flex items-end">
@@ -38,7 +38,7 @@ export default function AboutUs3() {
           </div>
         </div>
         <div className="md:w-[29.219vw] md:h-[14.219vw] w-[90.93vw] h-[60.884vw] md:ml-[4.5vw]">
-          <h1 className="font-bold text-[9.756vw] md:text-[2.604vw] text-[#F65050] mt-[4vw]">
+          <h1 className="font-bold text-[9.756vw] md:text-[2.604vw] text-[#F65050] md:mt-0 mt-[3vw]">
             About us
           </h1>
           <p className="text-[3.902vw] md:text-[1.042vw] text-justify">
@@ -46,7 +46,7 @@ export default function AboutUs3() {
             tincidunt id. Sed scelerisque in eu auctor. Lectus enim id et eget
             risus est ipsum morbi sit. Cursus tortor in et at viverra fermentum.
           </p>
-          <p className="mt-[2vw] text-[3.902vw] md:text-[1.042vw] text-justify">
+          <p className="mt-[1vw] text-[3.902vw] md:text-[1.042vw] text-justify">
             Orci tellus fames pharetra elit fermentum magna morbi donec.
             Pulvinar quis blandit sollicitudin ut lectus cursus. Ipsum luctus
             duis vitae venenatis. Amet.
