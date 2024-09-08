11f91d8a6113f55d8a2e0762b00338a9ec2d8491
ryanlukito <ryanlukito13@gmail.com>
2024-09-08 10:00:57 +0700

    msg: update all teams

--- template_library/src/app/sections/teams/teamsVariant1.tsx
+++ template_library/src/app/sections/teams/teamsVariant1.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 const sources = [
   {
     src: "/image/teams/Avatar1.png",
-    width: "w-[4.167vw]",
-    height: "h-[4.167vw]",
+    width: "md:w-[4.167vw] w-[18.605vw]",
+    height: "md:h-[4.167vw] h-[18.605vw]",
     title: "Theresa Webb",
     text1: "Application Support Analyst Lead",
     text2:
@@ -12,24 +12,24 @@ const sources = [
   },
   {
     src: "/image/teams/Avatar2.png",
-    width: "w-[4.167vw]",
-    height: "h-[4.167vw]",
+    width: "md:w-[4.167vw] w-[18.605vw]",
+    height: "md:h-[4.167vw] h-[18.605vw]",
     title: "Courtney Henry",
     text1: "Director, Undergraduate Analytics and Planning",
     text2: "Lead engineering teams at Figma, Pitch, and Protocol Labs.",
   },
   {
     src: "/image/teams/Avatar3.png",
-    width: "w-[4.167vw]",
-    height: "h-[4.167vw]",
+    width: "md:w-[4.167vw] w-[18.605vw]",
+    height: "md:h-[4.167vw] h-[18.605vw]",
     title: "Albert Flores",
     text1: "Career Educator",
     text2: "Former PM for Linear, Lambda School, and On Deck.",
   },
   {
     src: "/image/teams/Avatar4.png",
-    width: "w-[4.167vw]",
-    height: "h-[4.167vw]",
+    width: "md:w-[4.167vw] w-[18.605vw]",
+    height: "md:h-[4.167vw] h-[18.605vw]",
     title: "Marvin McKinney",
     text1: "Co-op & Internships Program & Operations Manager",
     text2: "Former frontend dev for Linear, Coinbase, and Postscript.",
@@ -38,20 +38,22 @@ const sources = [
 
 export default function teams1() {
   return (
-    <section className="w-full min-h-[36.875vw] p-8 flex flex-col justify-center items-center bg-white text-gray-800 relative">
-      <div className="w-[62.5vw] h-[24.375vw] flex flex-col items-center">
-        <div className="w-[40vw] h-[6.458vw] flex flex-col items-center justify-center  ">
-          <h1 className="font-bold text-[1.875vw]">Meet the Team</h1>
-          <p className="text-[1.042vw] mt-[1vw]">
+    <section className="w-full md:min-h-[36.875vw] md:aspect-ratio[1920/1080] aspect-ratio[430/1702] p-8 flex flex-col justify-center items-center bg-white text-gray-800 relative">
+      <div className="md:w-[62.5vw] md:h-[24.375vw] w-[88.605vw] h-[347.907vw] flex flex-col items-center justify-between">
+        <div className="md:w-[40vw] md:h-[6.458vw] w-[88.605vw] h-[42.791vw] flex flex-col items-center justify-between md:justify-center  ">
+          <h1 className="font-bold text-[8.372vw] md:text-[1.875vw]">
+            Meet the Team
+          </h1>
+          <p className="text-[4.651vw] md:text-[1.042vw] mt-0 md:mt-[1vw] text-center text-[#667085]">
             Lorem ipsum dolor sit amet consectetur. Senectus ultrices tortor
             massa quis sagittis. Ultrices malesuada adipiscing parturient
             vivamus. Tristique ac.
           </p>
         </div>
-        <div className="w-[62.5vw] h-[15.833vw] flex flex-row items-center justify-between mt-[2vw]">
+        <div className="md:w-[62.5vw] md:h-[15.833vw] w-[64.186vw] h-[305.116vw] flex flex-col md:flex-row items-center justify-between mt-[2vw]">
           {sources.map((client, index) => (
             <div
-              className="w-[14.375vw] h-[15.833vw] flex flex-col items-center bg-[#F9F9F9] rounded-[0.521vw] border-[0.052vw] border-[#D4D4D4] p-[1.25vw]"
+              className="md:w-[14.375vw] md:h-[15.833vw] w-[64.186vw] h-[70.698vw] flex flex-col items-center bg-[#F9F9F9] rounded-[2.326vw] md:rounded-[0.521vw] border-[0.052vw] border-[#D4D4D4] p-[3vw] md:p-[1.25vw]"
               key={index}
             >
               <Image
@@ -59,15 +61,15 @@ export default function teams1() {
                 alt="Ornament Background"
                 width={10000}
                 height={10000}
-                className={`${client.width} ${client.height} object-cover`}
+                className={`${client.width} ${client.height} object-cover md:mt-0 mt-[3vw]`}
               />
-              <p className="font-bold text-[0.938vw] mt-[0.5vw]">
+              <p className="font-bold text-[4.186vw] md:text-[0.938vw] mt-[3vw] md:mt-[0.5vw]">
                 {client.title}
               </p>
-              <p className="text-[#407BFF] text-[0.833vw] text-center mt-[0.5vw]">
+              <p className="text-[#407BFF] text-[3.721vw] md:text-[0.833vw] text-center mt-[0.5vw]">
                 {client.text1}
               </p>
-              <p className="text-[0.833vw] text-[#667085] text-center mt-[0.5vw]">
+              <p className="text-[3.721vw] md:text-[0.833vw] text-[#667085] text-center mt-[0.5vw]">
                 {client.text2}
               </p>
             </div>
--- template_library/src/app/sections/teams/teamsVariant2.tsx
+++ template_library/src/app/sections/teams/teamsVariant2.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 const sources = [
   {
     src: "/image/teams/Avatar5.png",
-    width: "w-[8.229vw]",
-    height: "h-[8.229vw]",
+    width: "w-[36.744vw] md:w-[8.229vw]",
+    height: "h-[36.744vw] md:h-[8.229vw]",
     title: "Courtney Henry",
     text1: "FOUNDER",
     text2:
@@ -12,8 +12,8 @@ const sources = [
   },
   {
     src: "/image/teams/Avatar6.png",
-    width: "w-[8.229vw]",
-    height: "h-[8.229vw]",
+    width: "w-[36.744vw] md:w-[8.229vw]",
+    height: "h-[36.744vw] md:h-[8.229vw]",
     title: "Ian Kent",
     text1: "CO-FOUNDER",
     text2:
@@ -21,8 +21,8 @@ const sources = [
   },
   {
     src: "/image/teams/Avatar6.png",
-    width: "w-[8.229vw]",
-    height: "h-[8.229vw]",
+    width: "w-[36.744vw] md:w-[8.229vw]",
+    height: "h-[36.744vw] md:h-[8.229vw]",
     title: "Ian Kent",
     text1: "CO-FOUNDER",
     text2:
@@ -32,14 +32,16 @@ const sources = [
 
 export default function teams2() {
   return (
-    <section className="w-full min-h-[35.833vw] p-8 flex flex-col justify-center items-center bg-white text-gray-800 relative">
-      <div className="w-[40vw] h-[2.292vw] flex flex-col items-center justify-center">
-        <h1 className="text-[2.604vw] font-bold">Our Team</h1>
+    <section className="w-full md:min-h-[35.833vw] md:aspect-ratio[1920/1080] aspect-ratio[430/1478] p-8 flex flex-col justify-center items-center bg-white text-gray-800 relative">
+      <div className="md:w-[40vw] md:h-[2.292vw] w-full h-[10.233vw] flex flex-col items-center justify-center">
+        <h1 className="text-[11.628vw] md:text-[2.604vw] font-bold">
+          Our Team
+        </h1>
       </div>
-      <div className="w-[67.083vw] h-[17.917vw] flex flex-row justify-between mt-[2vw]">
+      <div className="md:w-[67.083vw] md:h-[17.917vw] w-[87.442vw] h-[277.209vw] flex flex-col md:flex-row justify-between mt-[2vw]">
         {sources.map((client, index) => (
           <div
-            className="relative w-[19.583vw] h-[17.917vw] flex flex-col items-center"
+            className="relative md:w-[19.583vw] md:h-[17.917vw] w-full h-[80vw] flex flex-col items-center"
             key={index}
           >
             <Image
@@ -49,11 +51,13 @@ export default function teams2() {
               height={10000}
               className={`${client.width} ${client.height} object-cover`}
             />
-            <p className="font-bold text-[1.25vw] mt-[0.5vw]">{client.title}</p>
-            <p className="text-[#407BFF] text-[0.833vw] text-center mt-[0.5vw]">
+            <p className="font-bold text-[5.581vw] md:text-[1.25vw] mt-[2vw] md:mt-[0.5vw]">
+              {client.title}
+            </p>
+            <p className="text-[#407BFF] text-[3.721vw] md:text-[0.833vw] text-center mt-[0.5vw]">
               {client.text1}
             </p>
-            <p className="text-[0.833vw] text-[#667085] text-center mt-[1vw]">
+            <p className="text-[3.721vw] md:text-[0.833vw] text-[#667085] text-center mt-[4vw] md:mt-[1vw]">
               {client.text2}
             </p>
             <div className="flex flex-row absolute bottom-0">
@@ -62,14 +66,14 @@ export default function teams2() {
                 alt="Ornament Background"
                 width={10000}
                 height={10000}
-                className="w-[1.042vw] h-[1.042vw] mr-[0.7vw]"
+                className="md:w-[1.042vw] md:h-[1.042vw] w-[4.651vw] h-[4.651vw] mr-[2vw] md:mr-[0.7vw]"
               />
               <Image
                 src="/image/teams/linkedin.png"
                 alt="Ornament Background"
                 width={10000}
                 height={10000}
-                className="w-[1.042vw] h-[1.042vw]"
+                className="md:w-[1.042vw] md:h-[1.042vw] w-[4.651vw] h-[4.651vw]"
               />
             </div>
           </div>
